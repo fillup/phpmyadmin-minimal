f13fa2a844293a181b8879e3db0db457b14ef7fa
Phillip Shipley <phillip.shipley@gmail.com>
2015-08-10 10:40:56 -0400

    Add env var support for blowfish secret

--- config.inc.php
+++ config.inc.php
@@ -14,7 +14,8 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$blowfishSecret = getenv('BLOWFISH_SECRET') ?: null;
+$cfg['blowfish_secret'] = $blowfishSecret; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /*
  * Servers configuration
